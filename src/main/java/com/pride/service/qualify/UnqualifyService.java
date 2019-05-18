c55a416e677a8ed907a38b52f5bcdc4aeb5c68bc
yuanchuan <380029172@qq.com>
2019-05-18 18:14:48 +0800

    大家最新进度，没有报错的版本

--- src/main/java/com/pride/service/qualify/UnqualifyService.java
+++ src/main/java/com/pride/service/qualify/UnqualifyService.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-@Service
+
 public interface UnqualifyService
 {
     List<UnqualifyApplyVO> queryUnqualifyApplyVOList();
